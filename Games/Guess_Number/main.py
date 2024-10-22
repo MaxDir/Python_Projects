0781c70cb814b48900bedcbb8957bb6ecea16efc
MaxDir <maksim.bulkin99@gmail.com>
2024-10-22 10:09:22 +0400

    Добавления основного цикла игры while

--- Games/Guess_Number/main.py
+++ Games/Guess_Number/main.py
@@ -4,3 +4,15 @@ print('Игра угадай число')
 print('Загадано число от 1 до 100')
 
 random_number = randint(1, 100)
+print(random_number)
+
+flag_game = True
+while flag_game:
+    user_number = int(input('Введите число: '))
+    if user_number == random_number:
+        print(f'Вы угадали загаданное число {user_number}!')
+        flag_game = False
+    elif user_number > random_number:
+        print('Ваше число больше, загаданного')
+    elif user_number < random_number:
+        print('Ваше число меньше загаданного')
