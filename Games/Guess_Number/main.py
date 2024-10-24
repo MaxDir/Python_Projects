2747c654230343ec7532edbd52a61758ba72521f
Maxich <maksim.bulkin99@gmail.com>
2024-10-24 22:32:31 +0400

    Добавлен if для вывода ошибки, если введённое число не соответствует функции check_input()

--- Games/Guess_Number/main.py
+++ Games/Guess_Number/main.py
@@ -16,11 +16,15 @@ print(random_number)
 
 flag_game = True
 while flag_game:
-    user_number = int(input('Введите число: '))
-    if user_number == random_number:
-        print(f'Вы угадали загаданное число {user_number}!')
-        flag_game = False
-    elif user_number > random_number:
-        print('Ваше число больше, загаданного')
-    elif user_number < random_number:
-        print('Ваше число меньше загаданного')
+    user_number = input('Введите число: ')
+
+    if check_input(user_number):
+        if int(user_number) == random_number:
+            print(f'Вы угадали загаданное число {user_number}!')
+            flag_game = False
+        elif int(user_number) > random_number:
+            print('Ваше число больше, загаданного')
+        elif int(user_number) < random_number:
+            print('Ваше число меньше загаданного')
+    else:
+        print('[Ошибка] Введите число от 1 до 100.')
