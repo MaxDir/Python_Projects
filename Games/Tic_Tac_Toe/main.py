d3716c573e3bfe8df8f7081d3063c92d5196ad04
Maxich <maksim.bulkin99@gmail.com>
2024-11-04 19:44:18 +0400

    Функция draw_cells() для вывода ячеек

---
+++ Games/Tic_Tac_Toe/main.py
@@ -0,0 +1,10 @@
+
+def draw_cells():
+    print(f' {cell[0]} | {cell[1]} | {cell[2]}')
+    print('---|---|---')
+    print(f' {cell[3]} | {cell[4]} | {cell[5]}')
+    print('---|---|---')
+    print(f' {cell[6]} | {cell[7]} | {cell[8]}')
+
+cell = ['', '', '', '', '', '', '', '', '']
+
